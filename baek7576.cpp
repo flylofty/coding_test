d013429a37c4f53f7bf683c459da6051bd0f97ff
JH <ljh596088@gmail.com>
2020-07-19 00:12:39 +0900

    백준 7576 토마토

---
+++ baek7576.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+int N, M; //행, 렬
+vector<vector<int>> box;
+queue<pair<int, int>> mature;
+queue<pair<int, int>> temp;
+int dir[4][2] = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
+int notMature = 0;
+
+void input() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> M >> N;
+	box.resize(N + 1, vector<int>(M + 1));
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= M; ++j) {
+			cin >> box[i][j];
+
+			if (box[i][j] == 1)
+				temp.push(pair<int, int>(i, j));
+			else if(box[i][j] == 0)
+				notMature++;
+		}
+	}
+}
+
+void BFS() {
+
+	mature = temp;
+
+	while (!mature.empty()) {
+		int cur_x = mature.front().first;
+		int cur_y = mature.front().second;
+		mature.pop();
+		temp.pop();
+
+		for (int i = 0; i < 4; ++i) {
+			int next_x = cur_x + dir[i][0];
+			int next_y = cur_y + dir[i][1];
+
+			if ((next_x >= 1 && next_x <= N) && (next_y >= 1 && next_y <= M) && box[next_x][next_y] == 0) {
+				notMature--;
+				box[next_x][next_y] = 1;
+				temp.push(pair<int, int>(next_x, next_y));
+			}
+		}
+	}
+
+}
+
+int main() {
+
+	input();
+
+	int cnt = -1;
+	while (!temp.empty()) {
+		cnt++;
+		BFS();
+	}
+
+	cout.tie(0);
+	if (notMature == 0)
+		cout << cnt << "\n";
+	else
+		cout << "-1\n";
+
+	return 0;
+}
